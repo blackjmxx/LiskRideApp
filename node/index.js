7e5bfcf4388767f578ca44d1f799f52f46fcb8af
blackjmxx <alandou.jeanmichel@gmail.com>
2020-12-03 16:53:18 +0100

    add node feature

---
+++ node/index.js
@@ -0,0 +1,35 @@
+const { Application, genesisBlockDevnet, configDevnet } = require('lisk-sdk');
+
+const AddAccountInfoTransaction = require('../transactions/add-account-info');
+const RegisterTravelTransaction = require('../transactions/register-travel');
+const BookTravelTransaction = require('../transactions/book-travel');
+const StartTravelransaction = require('../transactions/start-travel');
+const EndTravelTransaction = require('../transactions/end-travel');
+
+const { ExtendedHTTPApiModule } = require('@moosty/lisk-extended-api');
+
+
+configDevnet.app.label = 'lisk-ride';
+configDevnet.modules.http_api.access.public = true;
+// configDevnet.components.storage.host = 'db';
+const app = new Application(genesisBlockDevnet, configDevnet);
+
+app.registerTransaction(AddAccountInfoTransaction);
+app.registerTransaction(RegisterTravelTransaction);
+app.registerTransaction(BookTravelTransaction);
+app.registerTransaction(StartTravelransaction);
+app.registerTransaction(EndTravelTransaction);
+
+app.registerModule(ExtendedHTTPApiModule, {
+    port: 2020,
+    limit: 1000,
+    assets: ['destination', 'pickUpLocation', 'pickUpDate', 'availableSeatCount', 'pricePerSeat', 'carId']
+  });
+
+app
+    .run()
+    .then(() => app.logger.info('App started...'))
+    .catch(error => {
+        console.error('Faced error in application', error);
+        process.exit(0);
+    });
---
+++ transactions/add-account-info.js
@@ -0,0 +1,96 @@
+const {
+    BaseTransaction,
+    TransactionError,
+    utils
+} = require('@liskhq/lisk-transactions');
+
+
+class AddAccountInfoTransaction extends BaseTransaction {
+
+    static get TYPE () {
+        return 30;
+    }
+
+    static get FEE () {
+        return '0';
+    };
+
+    async prepare(store) {
+        await store.account.cache([
+            {
+                address: this.asset.carId,
+            },
+            {
+                address: this.senderId,
+            }
+        ]);
+    }
+
+    validateAsset() {
+        const errors = [];
+        if (!this.asset.carId || typeof this.asset.carId !== 'string') {
+            errors.push(
+                new TransactionError(
+                    'Invalid "asset.carId" defined on transaction',
+                    this.id,
+                    '.asset.carId',
+                    this.asset.carId
+                )
+            );
+        }
+        if (!this.asset.numberPlate || typeof this.asset.numberPlate !== 'string') {
+            errors.push(
+                new TransactionError(
+                    'Invalid "asset.numberPlate" defined on transaction',
+                    this.id,
+                    '.asset.numberPlate',
+                    this.asset.numberPlate
+                )
+            );
+        }
+        if (!this.asset.carModel || typeof this.asset.carModel !== 'string') {
+            errors.push(
+                new TransactionError(
+                    'Invalid "asset.carModel" defined on transaction',
+                    this.id,
+                    '.asset.carModel',
+                    this.asset.carModel,
+                    'A string value'
+                )
+            );
+        }
+        return errors;
+    }
+
+    applyAsset(store) {
+        const errors = [];
+        const user = store.account.get(this.asset.carId);
+
+        const updatedUserAccount = {
+          ...user,
+            asset: {
+              senderId: this.senderId,
+              numberPlate: this.asset.numberPlate,
+              carModel: this.asset.carModel,
+              email:this.asset.email
+          }
+        };
+        
+        store.account.set(user.address, updatedUserAccount);
+        return errors;
+      }
+
+    undoAsset(store) {
+        const errors = [];
+
+        /* --- Revert user car info --- */
+        const user = store.account.get(this.asset.carId);
+        const originalUserAccount = { ...user, asset: {...this.user.asset, numberPlate:null, carModel:null} };
+        store.account.set(user.address, originalUserAccount);
+        
+        return errors;
+    }
+
+}
+
+module.exports = AddAccountInfoTransaction;
